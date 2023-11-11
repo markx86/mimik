8c19c5e2d930d39fb89d9dad96d364ff451f7aba
markx86 <25851052+markx86@users.noreply.github.com>
2023-11-11 22:51:02 +0100

    util: add struct utils (offsetof, typeof, containerof)

---
+++ include/util/struct.h
@@ -0,0 +1,10 @@
+#ifndef MIMIK_STRUCT_H
+#define MIMIK_STRUCT_H
+
+#include <types.h>
+
+#define typeof __typeof__
+#define offsetof(type, member) ((size_t) &((type*) NULL)->member)
+#define containerof(ptr, type, member) ((type*)(((char*) ptr) - offsetof(type, member)))
+
+#endif
