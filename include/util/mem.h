3d0ee35757e6157f7a32b59ad1bd3691cb9c0791
markx86 <25851052+markx86@users.noreply.github.com>
2023-11-11 22:49:04 +0100

    util: add mem_copy and mem_copy_reverse

--- include/util/mem.h
+++ include/util/mem.h
@@ -4,5 +4,7 @@
 #include <types.h>
 
 void mem_set(void* p, uint8_t c, size_t s);
+void mem_copy(void* dst, void* src, size_t sz);
+void mem_copy_reverse(void* dst, void* src, size_t sz);
 
 #endif
--- util/mem.c
+++ util/mem.c
@@ -2,7 +2,18 @@
 
 void mem_set(void* p, uint8_t c, size_t s) {
 	uint8_t* b = p;
-	while (s-- > 0) {
+	while (s-- > 0)
 		*(b++) = c;
-	}
+}
+
+void mem_copy(void* dst, void* src, size_t sz) {
+	uint8_t *bdst = dst, *bsrc = src;
+	while (sz-- > 0)
+		*(bdst++) = *(bsrc++);
+}
+
+void mem_copy_reverse(void* dst, void* src, size_t sz) {
+	uint8_t *bdst = dst, *bsrc = src;
+	while (sz-- > 0)
+		bdst[sz-1] = bsrc[sz-1];
 }
