542c9856cb72e1df5c87ab79f63d00d5356e99f6
markx86 <25851052+markx86@users.noreply.github.com>
2023-11-11 22:13:36 +0100

    structs: add linked list data structure

---
+++ include/structs/list.h
@@ -0,0 +1,19 @@
+#ifndef MIMIK_LIST_H
+#define MIMIK_LIST_H
+
+#include <util/struct.h>
+
+struct list {
+	struct list* prev;
+	struct list* next;
+};
+
+void list_init(struct list* list);
+void list_insert(struct list* list, struct list* elem);
+void list_remove(struct list* elem);
+size_t list_length(struct list* list);
+
+#define list_for_each(pos, head, link) for (pos = containerof((head)->next, typeof(*pos), link); &pos->link != (head); pos = containerof(pos->link.next, typeof(*pos), link))
+#define list_for_each_reverse(pos, head, link) for (pos = containerof((head)->prev, typeof(*pos), link); &pos->link != (head); pos = containerof(pos->link.prev, typeof(*pos), link))
+
+#endif
