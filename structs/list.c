2ba00ed19da091691ba1a02821079f243acf1955
Mark <25851052+markx86@users.noreply.github.com>
2024-01-10 19:22:39 +0100

    util: refactor and add assert guards to list impl

--- structs/list.c
+++ structs/list.c
@@ -1,13 +1,17 @@
 #include <structs/list.h>
+#include <assert.h>
 
 void
 list_init(struct list* list) {
+  ASSERT(list != NULL);
   list->next = list;
   list->prev = list;
 }
 
 void
 list_insert(struct list* list, struct list* elem) {
+  ASSERT(list != NULL);
+  ASSERT(elem != NULL);
   elem->prev = list;
   elem->next = list->next;
   list->next = elem;
@@ -16,6 +20,7 @@ list_insert(struct list* list, struct list* elem) {
 
 void
 list_remove(struct list* elem) {
+  ASSERT(elem != NULL);
   elem->prev->next = elem->next;
   elem->next->prev = elem->prev;
   elem->prev = NULL;
@@ -24,8 +29,21 @@ list_remove(struct list* elem) {
 
 size_t
 list_length(struct list* list) {
+  ASSERT(list != NULL);
   size_t len = 0;
   for (struct list* elem = list->next; elem != list; elem = elem->next)
     ++len;
   return len;
+}
+
+void
+list_join(struct list* list, struct list* other) {
+  ASSERT(list != NULL);
+  ASSERT(list != other);
+  if (list_is_empty(other))
+    return;
+  other->next->prev = list;
+  other->prev->next = list->next;
+  list->next->prev = other->prev;
+  list->next = other->next;
 }
