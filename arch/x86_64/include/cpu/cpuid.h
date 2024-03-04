d1e8641e315fa984c98eb722c2ace0a99bc222ac
Mark <25851052+markx86@users.noreply.github.com>
2024-03-04 12:42:17 +0100

    cpu: x86_64: add cpuid utility

---
+++ arch/x86_64/include/cpu/cpuid.h
@@ -0,0 +1,14 @@
+#ifndef MIMIK_CPU_H
+#define MIMIK_CPU_H
+
+#include <types.h>
+#include <util/compiler.h>
+
+static inline void cpuid(uint32_t code, uint32_t out[4]) {
+	asm(
+		"cpuid"
+		: "=a"(out[0]), "=b"(out[1]), "=c"(out[2]), "=d"(out[3])
+		: "a"(code));
+}
+
+#endif
