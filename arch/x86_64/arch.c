93d5c52aa5c40911750ac95056612ba90f7b97c4
Mark <25851052+markx86@users.noreply.github.com>
2024-03-25 21:29:12 +0100

    cpu: x86_64: add support for 8259 PIC

--- arch/x86_64/arch.c
+++ arch/x86_64/arch.c
@@ -1,8 +1,11 @@
 #include <arch.h>
 #include <cpu/gdt.h>
+#include <cpu/pic.h>
 
 status_t
 arch_init(struct bootinfo* bootinfo) {
   gdt_load();
+  pic_init(&bootinfo->arch);
+  pic_mask(0x10);
   return SUCCESS;
 }
---
+++ arch/x86_64/include/cpu/pic.h
@@ -0,0 +1,11 @@
+#ifndef MIMIK_PIC_H
+#define MIMIK_PIC_H
+
+#include <boot/bootinfo.h>
+
+void pic_ack(uint8_t irq);
+void pic_mask(uint8_t irq);
+void pic_unmask(uint8_t irq);
+status_t pic_init(struct bootinfo_arch* bootinfo);
+
+#endif
