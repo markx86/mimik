c9d07a0aa9168f2dc50cd0c4fc87f93983358da6
Mark <25851052+markx86@users.noreply.github.com>
2024-11-04 23:53:52 +0100

    build: x86_64: fix linker script
    Add .lbss, .ldata, .lrodata, .ltext to linker script for x86_64.
    Add `_entry_*addr_start` and `_entry_*addr_end` symbols.

--- arch/x86_64/include/mem/layout.h
+++ arch/x86_64/include/mem/layout.h
@@ -1,7 +1,10 @@
 #ifndef MIMIK_LAYOUT_H
 #define MIMIK_LAYOUT_H
 
-#define _(x) extern char _kernel_##x##_start[0], _kernel_##x##_end[0]
+extern const char _entry_paddr_start[0], _entry_vaddr_start[0];
+extern const char _entry_paddr_end[0], _entry_vaddr_end[0];
+
+#define _(x) extern const char _kernel_##x##_start[0], _kernel_##x##_end[0]
 
 _(paddr);
 _(vaddr);
@@ -15,6 +18,11 @@ _(data);
 
 #define LAYOUT_PADDR_START ((addr_t)_kernel_paddr_start)
 #define LAYOUT_PADDR_END   ((addr_t)_kernel_paddr_end)
+#define LAYOUT_ENTRY_PADDR_START ((addr_t)_entry_vaddr_start)
+#define LAYOUT_ENTRY_PADDR_END   ((addr_t)_entry_vaddr_end)
+
+#define LAYOUT_ENTRY_VADDR_START ((addr_t)_entry_vaddr_start)
+#define LAYOUT_ENTRY_VADDR_END   ((addr_t)_entry_vaddr_end)
 
 #define LAYOUT_VADDR_START ((addr_t)_kernel_vaddr_start)
 #define LAYOUT_VADDR_END   ((addr_t)_kernel_vaddr_end)
@@ -29,5 +37,7 @@ _(data);
 #define LAYOUT_DATA_END   ((addr_t)_kernel_data_end)
 
 #define LAYOUT_HEAP_START 0xffff800000000000
+
+#define LAYOUT_PHYSMAP_BASE 0xfffff80000000000
 
 #endif
